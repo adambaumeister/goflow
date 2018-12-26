6168e6cd1c88d080cb735af1a422713db0f16dbf
adam <abigserve@gmail.com>
2018-12-26 16:26:55 +1100

    Help string

--- api/commands.go
+++ api/commands.go
@@ -9,15 +9,36 @@ import (
 )
 
 type Commands struct {
-	Paths map[string]*Path
+	Paths      map[string]Path
+	HelpString string
 }
 
-type Path struct {
-	Url  string
-	Data []byte
+type Path interface {
+	Get()
+	Help() string
 }
 
-func (p *Path) Get() {
+func (c *Commands) Get() {
+	s := "Goflow command line client help\n------------------\n\n"
+	for cmd, p := range c.Paths {
+		s = s + fmt.Sprintf("%v			: %v\n", cmd, p.Help())
+	}
+	fmt.Printf(s)
+}
+func (c *Commands) Help() string {
+	return "Display this help message."
+}
+
+/*
+HTTP Path do a call to the API to present their results
+*/
+type HttpPath struct {
+	Url        string
+	Data       []byte
+	HelpString string
+}
+
+func (p *HttpPath) Get() {
 	resp, err := http.Get("http://127.0.0.1:8880" + p.Url)
 	if err != nil {
 		panic(err)
@@ -28,14 +49,21 @@ func (p *Path) Get() {
 	fmt.Printf("%v\n", jm.Msg)
 }
 
+func (p *HttpPath) Help() string {
+	return p.HelpString
+}
+
 func (c *Commands) Parse() {
-	c.Paths = make(map[string]*Path)
+	c.Paths = make(map[string]Path)
 
 	// Setup the routes
-	testPath := Path{
-		Url: "/status",
+	testPath := HttpPath{
+		Url:        "/status",
+		HelpString: "Displays the status of configured Backends",
 	}
+
 	c.Paths["status"] = &testPath
+	c.Paths["help"] = c
 	if len(os.Args) > 1 {
 		c.Paths[os.Args[1]].Get()
 		os.Exit(0)
