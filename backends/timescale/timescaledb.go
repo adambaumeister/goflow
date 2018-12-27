6eeb0c4cab58b28972b0c05b0042df3717b813fd
adam <abigserve@gmail.com>
2018-12-27 15:41:16 +1100

    Changed columns in timescaledb to be inet type, easier for frontend to handle

--- backends/timescale/timescaledb.go
+++ backends/timescale/timescaledb.go
@@ -19,7 +19,7 @@ const TEST_QUERY = `SELECT MIN(last_switched) as minday, MAX(last_switched) as m
 ) AS fps
  FROM goflow_records;`
 
-const PRUNE_QUERY = "DELETE FROM goflow_records WHERE last_switched <= DATE_SUB(CURDATE(), INTERVAL %v DAY)"
+const PRUNE_QUERY = "DELETE FROM goflow_records WHERE last_switched <= NOW() - INTERVAL '%v DAYS';"
 
 type Tsdb struct {
 	Dbname string
@@ -235,9 +235,9 @@ func (b *Tsdb) Init() {
 	}
 	datetimec := s.AddIntColumn(fields.TIMESTAMP, "last_switched", "TIMESTAMPTZ", "NOT NULL")
 	datetimec.Wrap = "to_timestamp(%v)"
-	s.AddIntColumn(fields.IPV4_SRC_ADDR, "src_ip", "integer", "DEFAULT NULL")
+	s.AddBinaryColumn(fields.IPV4_SRC_ADDR, "src_ip", "inet", "DEFAULT NULL")
 	s.AddIntColumn(fields.L4_SRC_PORT, "src_port", "integer", "NOT NULL")
-	s.AddIntColumn(fields.IPV4_DST_ADDR, "dst_ip", "integer", "DEFAULT NULL")
+	s.AddBinaryColumn(fields.IPV4_DST_ADDR, "dst_ip", "inet", "DEFAULT NULL")
 	s.AddIntColumn(fields.L4_DST_PORT, "dst_port", "integer", "NOT NULL")
 	s.AddIntColumn(fields.IN_BYTES, "in_bytes", "integer", "NOT NULL")
 	s.AddIntColumn(fields.IN_PKTS, "in_pkts", "integer", "NOT NULL")
@@ -316,6 +316,7 @@ func (b *Tsdb) Prune(interval string) {
 	db := b.connect()
 	_, err := db.Exec(fmt.Sprintf(PRUNE_QUERY, interval))
 	if err != nil {
+		fmt.Printf(fmt.Sprintf(PRUNE_QUERY, interval))
 		panic(err.Error())
 	}
 }
